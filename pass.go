304472ddf3882d767b61923a48c197e95e514afb
hos8 <jam39so@outlook.com>
2024-06-26 22:24:48 +0800

    Modify pass.go

--- pass.go
+++ pass.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Statement customer candidate detail center doctor join.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
