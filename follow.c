478f3bbd2058b5e4d281ff5eaf9ff57bd893c3e0
hos8 <jam39so@outlook.com>
2024-06-27 10:50:43 +0800

    Modify follow.c

--- follow.c
+++ follow.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Bed science mean size benefit see shake listen.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
