6b0330bc3918618724ac6feef1576a93c71b63af
hos8 <jam39so@outlook.com>
2024-07-11 18:11:06 +0800

    Modify thought.cpp

--- thought.cpp
+++ thought.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# This produce money.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
