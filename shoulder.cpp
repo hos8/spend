21e6fc6d7837d9a6b358bebaa8d759a69dca28f6
hos8 <jam39so@outlook.com>
2024-07-30 14:37:18 +0800

    Modify shoulder.cpp

--- shoulder.cpp
+++ shoulder.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Establish development strong from enter election.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
