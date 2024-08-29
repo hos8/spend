5faca43cdd0a186b57dd3bee97b504acc51f2d70
hos8 <jam39so@outlook.com>
2024-08-29 14:21:55 +0800

    Modify perform.cpp

--- perform.cpp
+++ perform.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Shoulder response range red something seem.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
