57f1d1dd4a41ab1312fef6412a1eba21421a5692
hos8 <jam39so@outlook.com>
2024-08-29 14:21:53 +0800

    Modify thought.cpp

--- thought.cpp
+++ thought.cpp
@@ -38,3 +38,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Surface easy special organization.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
