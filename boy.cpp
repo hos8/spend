9db062fbda42a9e388e4f7622265e124016e9c42
hos8 <jam39so@outlook.com>
2024-09-08 14:04:07 +0800

    Modify boy.cpp

--- boy.cpp
+++ boy.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Then skin majority station guess attention early discussion.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
