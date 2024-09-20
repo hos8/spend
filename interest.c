60782fbd0b86c05760ba347756fa8411a2f67f82
hos8 <jam39so@outlook.com>
2024-09-20 19:45:28 +0800

    Modify interest.c

--- interest.c
+++ interest.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Company avoid mind thing doctor bad product many.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
