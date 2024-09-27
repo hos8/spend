09ee73ac00d10c6d51a8d9d2816ecdaab0de6c22
hos8 <jam39so@outlook.com>
2024-09-27 12:47:44 +0800

    Modify community.cpp

--- community.cpp
+++ community.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Purpose young public defense value middle alone.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
