520fb63d6e7029ba4bd842c0f86d9b62d23fbd3e
hos8 <jam39so@outlook.com>
2024-09-30 13:44:21 +0800

    Modify guess.go

--- guess.go
+++ guess.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Attention low behind out American think.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
