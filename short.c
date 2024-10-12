ff2c89d427ef03bf8a7a37bb2b2e9a2714184873
hos8 <jam39so@outlook.com>
2024-10-12 10:08:45 +0800

    Modify short.c

--- short.c
+++ short.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Thing understand discussion system apply garden team.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
