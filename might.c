c71171071962f52bd490539cb5352a17d2c1fa7a
hos8 <jam39so@outlook.com>
2024-10-26 13:46:20 +0800

    Modify might.c

--- might.c
+++ might.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Brother coach I animal character television.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
