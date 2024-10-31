46a310c61dfc6badd2a29291ed00f16ca387b689
hos8 <jam39so@outlook.com>
2024-10-31 13:23:36 +0800

    Modify white.cpp

--- white.cpp
+++ white.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# If always difficult company reality model.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
