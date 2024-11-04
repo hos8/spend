c13caac6f3859eb29f06cbd9c62031c2c91f4ce8
hos8 <jam39so@outlook.com>
2024-11-04 12:55:38 +0800

    Modify certain.cpp

--- certain.cpp
+++ certain.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# In worker political rate hand.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
