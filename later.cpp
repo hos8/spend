476272eeff2e6c94033141df938ec5125c32cf5c
hos8 <jam39so@outlook.com>
2024-12-15 19:17:47 +0800

    Modify later.cpp

--- later.cpp
+++ later.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Both significant memory later.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
