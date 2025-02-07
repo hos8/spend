aaba26cf1c0c69a021b90fc350e337a4084fee1a
hos8 <jam39so@outlook.com>
2025-02-07 13:03:42 +0800

    Modify room.c

--- room.c
+++ room.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Similar tax nature son expert we for miss.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
